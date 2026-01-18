4b2fae3cd952de25e5cc978a348c6c0ee62a51aa
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2026-01-18 09:11:55 +0600

    clean-up

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,6 @@ use Inertia\Inertia;
 // })->name('dashboard');
 
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('user/dashboard', UserDashboardController::class)->name('user.dashboard');
-});
-
-Route::resource('todos', \App\Http\Controllers\TodoController::class)
-    ->only(['index', 'store', 'update', 'destroy']);
-
 require __DIR__ . '/settings.php';
 require __DIR__ . '/frontend.php';
-require __DIR__ . '/user.php';
 require __DIR__ . '/admin.php';
